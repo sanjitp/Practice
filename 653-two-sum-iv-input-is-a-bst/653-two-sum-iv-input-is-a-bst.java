db20dfc94ef4d251454fbe5fcf5391624570b31e
sanjitp <53243219+sanjitp@users.noreply.github.com>
2022-05-11 22:58:30 +0530

    Time: 3 ms (87.53%), Space: 42.3 MB (98.28%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -14,28 +14,17 @@
  * }
  */
 class Solution {
-    public void inorder(TreeNode root,ArrayList<Integer> res)
+    Set<Integer> set=new HashSet<>();
+    public boolean findTarget(TreeNode root, int k) {
+        return dfs(root,k);
+    }
+    public boolean dfs(TreeNode root,int k)
     {
         if(root==null)
-            return;
-        inorder(root.left,res);
-        res.add(root.val);
-        inorder(root.right,res);
-    }
-    public boolean findTarget(TreeNode root, int k) {
-        ArrayList<Integer> res=new ArrayList<>();
-          inorder(root,res);
-        int end=res.size()-1;
-        int i=0;
-        while(i<end)
-        {
-           if(res.get(i)+res.get(end)==k)
-               return true;
-            if(res.get(i)+res.get(end)>k)
-               end--;
-            else
-                i++;
-        }
-        return false;
+            return false;
+        if(set.contains(k-root.val))
+            return true;
+        set.add(root.val);
+        return dfs(root.left,k) || dfs(root.right,k);
     }
 }
