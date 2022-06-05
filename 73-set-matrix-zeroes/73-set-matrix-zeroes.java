3adac606b34b6547c14dacd15f6c4a39687e885c
sanjitp <53243219+sanjitp@users.noreply.github.com>
2022-06-05 15:33:13 +0530

    Time: 1 ms (93.39%), Space: 43.5 MB (97.63%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -2,28 +2,25 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int rows=matrix.length;
         int col=matrix[0].length;
-        int[] dummy1=new int[rows];
-        int[] dummy2=new int[col];
-        Arrays.fill(dummy1,-1);
-        Arrays.fill(dummy2,-1);
-        for(int i=0;i<rows;i++)
-        {
-            for(int j=0;j<col;j++)
-            {
-                if(matrix[i][j]==0){
-                    dummy1[i]=0;
-                    dummy2[j]=0;
-                }
-            }
-        }
+        int col0=1;
         
         for(int i=0;i<rows;i++)
         {
-            for(int j=0;j<col;j++)
+            if(matrix[i][0]==0)
+                col0=0;
+            for(int j=1;j<col;j++)
             {
-                if(dummy1[i]==0 || dummy2[j]==0)
-                    matrix[i][j]=0;
+                if(matrix[i][j]==0)
+                    matrix[i][0]=matrix[0][j]=0;
             }
+        }
+        
+        for(int i=rows-1;i>=0;i--){
+            for(int j=col-1;j>=1;j--)
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                    matrix[i][j]=0;
+        if(col0==0)
+            matrix[i][0]=0;
         }
     }
 }
