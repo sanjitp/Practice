fa26f2f585b3bc94da13bae1db51cc0fbcadbc89
sanjitp <53243219+sanjitp@users.noreply.github.com>
2022-06-24 17:28:33 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.java
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.java
@@ -31,24 +31,22 @@ public class Main {
 
 class Solution {
     int findMaxSum(int arr[], int n) {
-        int[] dp = new int[n+1];
-        for(int i=0;i<n;i++)
-            dp[i]=-1;
+        int prev=arr[0];
+        int prev2=0;
         
-        return helper(arr,n-1,dp);
+        for(int i=1;i<n;i++)
+        {
+            int pick=arr[i];
+            if(i>1)
+            pick+=prev2;
+            int notpick=0+prev;
+            int curri=Math.max(pick,notpick);
+            
+            prev2=prev;
+            prev=curri;
+        }
+        return prev;
     }
     
-    public int helper(int arr[],int n,int [] dp)
-    {
-        if(n==0)
-        return arr[n];
-        if(n<0)
-        return 0;
-        if(dp[n]!=-1)
-        return dp[n];
-        int pick=arr[n]+ helper(arr,n-2,dp);
-        int notpick= 0 + helper(arr,n-1,dp);
-        
-        return dp[n]= Math.max(pick,notpick);
-    }
+   
 }
