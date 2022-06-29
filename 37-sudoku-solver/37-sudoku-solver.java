bbd612e783c399507145c4f579eb45f938212c4b
sanjitp <53243219+sanjitp@users.noreply.github.com>
2022-06-29 15:21:03 +0530

    Time: 23 ms (35.30%), Space: 42.1 MB (26.44%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+      helper(board);  
+    }
+    
+    public boolean helper(char[][] board)
+    {
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board[i].length;j++)
+            {
+                if(board[i][j]=='.')
+                {
+                    for(char c='1';c<='9';c++)
+                    {
+                        if(valid(board,i,j,c)){
+                            board[i][j]=c;
+                          
+                            if(helper(board)== true)
+                            return true;
+                            else
+                            board[i][j]='.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public boolean valid(char[][] board,int row,int col,char c)
+    {
+        for(int i=0;i<board.length;i++)
+        {
+            if(board[i][col]==c)
+                return false;
+            if(board[row][i]==c)
+                return false;
+            if(board[3 * (row/3) + i/3][3 * (col/3) + i%3]==c)
+                return false;
+        }
+        return true;
+    }
+}
